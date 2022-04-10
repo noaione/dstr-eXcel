47691f602f85bf54f7aefee043b048e851f02fd9
noaione <noaione0809@gmail.com>
2022-04-11 05:02:26 +0700

    utils: Implement randomId generator

---
+++ src/utils.cpp
@@ -0,0 +1,11 @@
+#include <random>
+#include "utils.hpp"
+
+std::random_device rd;
+
+
+int randomId() {
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(1, 69420);
+    return dist(mt);
+}
---
+++ src/utils.hpp
@@ -0,0 +1,6 @@
+#ifndef EXCEL_UTILS_HPP
+#define EXCEL_UTILS_HPP
+
+int randomId();
+
+#endif /* EXCEL_UTILS_HPP */
