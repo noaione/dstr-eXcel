5731b0d7387a29cf9c4d066f8a9dea3afa69f50b
noaione <noaione0809@gmail.com>
2022-04-23 21:54:09 +0700

    utils: add wait_for_enter_key

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <random>
 #include "utils.hpp"
 
@@ -7,4 +8,12 @@ int randomId() {
     std::mt19937 mt(rd());
     std::uniform_real_distribution<double> dist(1, 69420);
     return dist(mt);
+}
+
+void wait_for_enter_key() {
+    std::cout << "[Press Enter to Continue]";
+    if (std::cin.peek() == '\n') {
+        std::cin.ignore();
+    }
+    while (std::cin.peek() != '\n');
 }
--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -2,5 +2,6 @@
 #define EXCEL_UTILS_UTILS_HPP
 
 int randomId();
+void wait_for_enter_key();
 
 #endif /* EXCEL_UTILS_UTILS_HPP */
